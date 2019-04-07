a00fdb0197fcf5d1ed68b2c9214da16a320efbd1
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2019-04-08 02:43:28 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type Props = {};
+type State = {
+  dataArray: Uint8Array;
+};
+
+class App extends Component<Props, State> {
+  state: State = {
+    dataArray: new Uint8Array()
+  };
+
+  componentDidMount() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
+      .then((stream) => {
+        let context = new AudioContext();
+        let input = context.createMediaStreamSource(stream);
+        let analyzer = context.createAnalyser();
+        input.connect(analyzer);
+
+        analyzer.fftSize = 4096;
+        console.log("sampleRate: ", context.sampleRate);
+
+        // E, F, F#, G, G#, A, A#, B, C, C#, D, D#  -- 2
+        // E, F, F#, G, G#, A, A#, B, C, C#, D, D#  -- 3
+        // E, F, F#, G, G#                          -- 4
+
+        setInterval(
+          () => {
+            let dataArray = new Uint8Array(analyzer.frequencyBinCount);
+            analyzer.getByteFrequencyData(dataArray);
+            this.setState({ dataArray: dataArray });
+          },
+          100
+        )
+      });
+  }
+
   render() {
+    let circles = [];
+    let maxFrequency = 0, maxFrequencyVolume = 0;
+    for (let i = 0; i < this.state.dataArray.length; i++) {
+      circles.push(<circle key={i} cx={i / 2} cy={this.state.dataArray[i] / 4} r={0.5}></circle>);
+
+      let frequency = 48000 * i / 4096;
+      if (frequency < 82) {
+        continue;
+      }
+
+      if (this.state.dataArray[i] > maxFrequencyVolume) {
+        maxFrequencyVolume = this.state.dataArray[i];
+        maxFrequency = frequency;
+      }
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Guitar Positions</h2>
+        <svg viewBox="0 0 200 100">
+          {circles}
+        </svg>
+        {maxFrequency} / {this.state.dataArray.length} {maxFrequencyVolume}
       </div>
     );
   }
