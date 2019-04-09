46d5f7ee499cb2ecc35efb1fb81a8874953bcf54
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2019-04-09 22:04:47 +0900

    Updates

---
+++ precache-manifest.e46cbdff6095a7ea0df583e27ca532c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cba2f36940ff38942f0",
+    "url": "/guitar-tones/static/css/main.d3068cd3.chunk.css"
+  },
+  {
+    "revision": "2cba2f36940ff38942f0",
+    "url": "/guitar-tones/static/js/main.6b44d480.chunk.js"
+  },
+  {
+    "revision": "4b3b28297a79debadb5f",
+    "url": "/guitar-tones/static/js/runtime~main.7f6330e7.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/guitar-tones/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aeb67b2984ca96f989d1da107194bc48",
+    "url": "/guitar-tones/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f66350c14a30c0b2fdc5f188a9c02341.js"
+  "/guitar-tones/precache-manifest.e46cbdff6095a7ea0df583e27ca532c5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/guitar-tones/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
